45a8cd84be10ba748feedfbd5fc9521a294e22b8
Amurdock6 <77793000+Amurdock6@users.noreply.github.com>
2022-06-26 15:48:59 -0600

    added drop down to nav for 'Coaching Services' button

--- src/js/nav.js
+++ src/js/nav.js
@@ -5,14 +5,11 @@ const menu = document.querySelector("#menu");
 const nav = document.querySelector("#nav");
 const burgerWrapper = document.querySelector("#logo-and-hamburger");
 
-
 burger.addEventListener('click', () => {
     if(menu.classList.contains('hidden')) {
         menu.classList.remove('hidden');
         menu.classList.add('flex');
-        menu.classList.add('text-center');
         menu.classList.add('w-full');
-        menu.classList.add('items-center');
         menu.classList.add('h-screen');
         nav.classList.add('h-screen');
         nav.classList.remove('items-center');
@@ -20,13 +17,10 @@ burger.addEventListener('click', () => {
         burgerWrapper.classList.remove('items-center');
         burger.classList.add('items-center');
         burger.classList.add('flex');
-
     } else {
         menu.classList.add('hidden');
         menu.classList.remove('flex');
-        menu.classList.remove('text-center');
         menu.classList.remove('w-full');
-        menu.classList.remove('items-center');
         menu.classList.remove('h-screen');
         nav.classList.remove('h-screen');
         nav.classList.add('items-center');
@@ -35,5 +29,17 @@ burger.addEventListener('click', () => {
         burger.classList.remove('items-center');
         burger.classList.remove('flex');
 
+    }
+});
+
+// Nav Drop Down on Click Listener
+const dropdown = document.querySelector("#dropdown");
+const dropdownContent = document.querySelector("#dropdwon-content")
+
+dropdown.addEventListener('click', () => {
+    if(dropdownContent.classList.contains('block')) {
+        dropdownContent.classList.remove('block');
+    } else {
+        dropdownContent.classList.add('block');
     }
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,9 @@ module.exports = {
       colors: {
         customteal: '#008080',
         customlightgray: '#D2D2D2',
+        customgray: '#ddd',
+        customdropdownback: '#f9f9f9',
+        customdarkteal: '#005050'
       },
       fontFamily: {
         Lora: ["Lora", "serif"]
