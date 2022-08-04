5d27253eeb96e41c1024a9e87350ba436d8e5d6a
Amurdock6 <77793000+Amurdock6@users.noreply.github.com>
2022-08-04 00:42:19 -0600

    added cookie that makes pop only show once per user seesion

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,29 +1,65 @@
 // Pop on Click Close/ Open logic
 const popXClose = document.querySelector("#popclosex");
-const closepop = document.querySelector("#close")
-const popupback = document.querySelector("#popup")
+const closepop = document.querySelector("#close");
+const popupback = document.querySelector("#popup");
 
 if(window.location.href == "http://localhost:5500/index.html#contactform") {
     popupback.classList.add('hidden');
 }
 
-popXClose.addEventListener('click', () => {
-    if(popupback.classList.contains('hidden')) {
-        popupback.classList.remove('hidden');
-        popupback.classList.add('block');
-    } else {
+/* Checks to see if cookie exist if it does then the pop up will not be showen to user */
+function getCookie(name) {
+    var dc = document.cookie;
+    var prefix = name + "=";
+    var begin = dc.indexOf("; " + prefix);
+    if (begin == -1) {
+        begin = dc.indexOf(prefix);
+        if (begin != 0) return null;
+    }
+    else
+    {
+        begin += 2;
+        var end = document.cookie.indexOf(";", begin);
+        if (end == -1) {
+        end = dc.length;
+        }
+    }
+    return decodeURI(dc.substring(begin + prefix.length, end));
+} 
+
+function checkCookie() {
+    
+    var myCookie = getCookie("poppedup");
+
+    if (myCookie == null) {
+        popXClose.addEventListener('click', () => {
+            if(popupback.classList.contains('hidden')) {
+                popupback.classList.remove('hidden');
+                popupback.classList.add('block');
+            } else {
+                popupback.classList.remove('block');
+                popupback.classList.add('hidden');
+            }
+        
+        });
+        
+        closepop.addEventListener('click', () => {
+            if(popupback.classList.contains('hidden')) {
+                popupback.classList.remove('hidden');
+                popupback.classList.add('block');
+            } else {
+                popupback.classList.remove('block');
+                popupback.classList.add('hidden');
+            }
+        });
+        
+        document.cookie = "poppedup=true";
+
+    }
+    else {
         popupback.classList.remove('block');
         popupback.classList.add('hidden');
     }
+}
 
-});
-
-closepop.addEventListener('click', () => {
-    if(popupback.classList.contains('hidden')) {
-        popupback.classList.remove('hidden');
-        popupback.classList.add('block');
-    } else {
-        popupback.classList.remove('block');
-        popupback.classList.add('hidden');
-    }
-});
+checkCookie();
